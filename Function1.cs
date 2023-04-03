8d88f4689d85fb84d72a01afedfb06c7c0d22d0c
JyothsnaVS <joshenoy@gmail.com>
2023-04-03 18:33:24 +1000

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using YNABReportGenerator.Models;
+using System.Text;
+using System.Net.Http.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace YNABReportGenerator
+{
+    public class Function1
+    {
+        private ILogger _log { get; set; }
+
+        [FunctionName("Function1")]
+        public async Task Run([TimerTrigger("0 8 * * 0")]TimerInfo myTimer, ILogger log)
+        //public async Task Run([TimerTrigger("*/1 *  * * *")] TimerInfo myTimer, ILogger log)
+        {
+            _log = log;
+
+            _log.LogInformation($"Generating YNAB expense report: {DateTime.Now}");
+            await RequestWeeklyYNABTransactions();
+        }
+
+        public async Task RequestWeeklyYNABTransactions()
+        {
+            string ynabEndPoint = Environment.GetEnvironmentVariable("YNABEndpoint");
+            string ynabAccessKey = Environment.GetEnvironmentVariable("YNABAccessKey");
+            string ynabBudgetId = Environment.GetEnvironmentVariable("YNABBudgetID");
+
+            string reportStartDate = DateTime.Now.AddDays(-7).ToString("yyyy-MM-dd");
+
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ynabAccessKey);
+
+            string ynabTransactions = await client.GetStringAsync(ynabEndPoint + ynabBudgetId + "/transactions?since_date=" + reportStartDate);
+
+            if (!String.IsNullOrEmpty(ynabTransactions))
+                await SendExpenseReportToDiscord(ynabTransactions);
+            else
+                _log.LogError($"YNAB transaction request failed.");
+        }
+
+        public async Task SendExpenseReportToDiscord(string content)
+        {
+            string webHookURL = System.Environment.GetEnvironmentVariable("WebHookURL");
+
+            TransactionQueryResult reportData = JsonConvert.DeserializeObject<TransactionQueryResult>(content);
+            ReportData report = new ReportData(reportData); 
+
+            List<object> fields = new List<object>{};
+            for (int i = 0; i < report.reportItems.Count; i++)
+            {
+                fields.Add(new
+                {
+                    name = report.reportItems[i].CategoryName,
+                    value = report.reportItems[i].Amount.ToString("C2"),
+                    inline = false
+                });
+            }
+
+            var webhookContent = new
+            {
+                embeds = new List<object>
+                {
+                    new
+                    {
+                        type = "rich",
+                        title="Weekly Expense report",
+                        fields = fields
+                    }
+                }
+            };
+
+            StringContent httpContent = new StringContent(JsonConvert.SerializeObject(webhookContent), Encoding.UTF8, "application/json");
+            
+            using HttpClient client = new();
+            var response = await client.PostAsync(webHookURL, httpContent);
+
+            if(!response.IsSuccessStatusCode)
+                _log.LogError($"discord webhook failure: {response.ReasonPhrase}");
+        }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YNABReportGenerator.Models
+{
+    public partial class TransactionQueryResult
+    {
+        [JsonProperty("data")]
+        public TransactionData Data { get; set; }
+    }
+
+    public class TransactionData
+    {
+        [JsonProperty("transactions")]
+        public List<Transaction> Transactions { get; set; }
+
+        [JsonProperty("server_knowledge")]
+        public long ServerKnowledge { get; set; }
+    }
+
+    public class Transaction
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("amount")]
+        public long Amount { get; set; }
+
+        [JsonProperty("memo")]
+        public string Memo { get; set; }
+
+        [JsonProperty("cleared")]
+        public string Cleared { get; set; }
+
+        [JsonProperty("approved")]
+        public bool Approved { get; set; }
+
+        [JsonProperty("flag_color")]
+        public string FlagColor { get; set; }
+
+        [JsonProperty("account_id")]
+        public string AccountId { get; set; }
+
+        [JsonProperty("payee_id")]
+        public string PayeeId { get; set; }
+
+        [JsonProperty("category_id")]
+        public string CategoryId { get; set; }
+
+        [JsonProperty("transfer_account_id")]
+        public string TransferAccountId { get; set; }
+
+        [JsonProperty("transfer_transaction_id")]
+        public string TransferTransactionId { get; set; }
+
+        [JsonProperty("matched_transaction_id")]
+        public string MatchedTransactionId { get; set; }
+
+        [JsonProperty("import_id")]
+        public string ImportId { get; set; }
+
+        [JsonProperty("import_payee_name")]
+        public string ImportPayeeName { get; set; }
+
+        [JsonProperty("import_payee_name_original")]
+        public string ImportPayeeNameOriginal { get; set; }
+
+        [JsonProperty("debt_transaction_type")]
+        public string DebtTransactionType { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool Deleted { get; set; }
+
+        [JsonProperty("account_name")]
+        public string AccountName { get; set; }
+
+        [JsonProperty("payee_name")]
+        public string PayeeName { get; set; }
+
+        [JsonProperty("category_name")]
+        public string CategoryName { get; set; }
+
+        [JsonProperty("subtransactions")]
+        public List<Subtransaction> Subtransactions { get; set; }
+    }
+
+    public class Subtransaction
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("transaction_id")]
+        public string TransactionId { get; set; }
+
+        [JsonProperty("amount")]
+        public long Amount { get; set; }
+
+        [JsonProperty("memo")]
+        public string Memo { get; set; }
+
+        [JsonProperty("payee_id")]
+        public string PayeeId { get; set; }
+
+        [JsonProperty("payee_name")]
+        public string PayeeName { get; set; }
+
+        [JsonProperty("category_id")]
+        public string CategoryId { get; set; }
+
+        [JsonProperty("category_name")]
+        public string CategoryName { get; set; }
+
+        [JsonProperty("transfer_account_id")]
+        public string TransferAccountId { get; set; }
+
+        [JsonProperty("transfer_transaction_id")]
+        public string TransferTransactionId { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool Deleted { get; set; }
+    }
+
+    public class ReportData
+    {
+       public List<ReportItem> reportItems { get; set; }
+
+        public ReportData()
+        {
+            this.reportItems = new List<ReportItem>();
+        }
+
+        public ReportData(TransactionQueryResult data)
+        {
+            this.reportItems = new List<ReportItem>();
+
+            if (data.Data.Transactions.Count > 0)
+            {
+                this.reportItems = data.Data.Transactions
+                                    .GroupBy(x => x.CategoryName)
+                                    .Select(g => new ReportItem
+                                    {
+                                        Amount = g.Sum(x => x.Amount)/-1000.00,
+                                        CategoryName = g.Key
+                                    })
+                                    .Where(g => g.Amount > 0).ToList();
+            }
+        }
+    }
+
+    public class ReportItem
+    {
+        public double Amount { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
